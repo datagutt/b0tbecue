65e2ab3b693afbe8d5d464bc5f854cdcdb441f74
nebkat <nebkat@teamhacksung.org>
2012-04-05 21:49:28 +0200

    log on/off

--- plugins/log.plugin.js
+++ plugins/log.plugin.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var logged = [];
+var enabled = {global: true, channels: []};
 function log(content, channel){
+	if (enabled.global == false) return;
+	if (enabled.channels[channel] == false) return;
 	var now = new Date();
 	var date = now.getDay() + '-' + now.getMonth() + '-' + now.getFullYear();
 	var folder = 'plugins/logs/';
@@ -23,11 +26,39 @@ function log(content, channel){
 	}); 
 }
 exports.init = function(plugins, bot){
+	bot.addCommand('log', 'on/off [channel]', 'Enables/disables logging', USER_LEVEL_OWNER);
 	plugins.listen('Log', 'topic', function(args){
 		log('[TOPIC] '+args.topic, args.channel);
 	});
 	plugins.listen('Log', 'command', function(args){
 		log('[COM] ['+args.channel+'] '+args.user+' ran command: '+args.command, args.channel);
+		if(args.command == 'log'){
+			if(args.arguments && args.arguments[1]){
+				var channel = args.arguments[1];
+			}
+			if(args.arguments && args.arguments[0]){
+				switch(args.arguments[0]){
+					case 'on':
+						if(!channel){
+							enabled.global = true;
+							IRC.message(args.channel, 'Logging enabled');
+						}else{
+							enabled.channels[channel] = true;
+							IRC.message(args.channel, 'Logging enabled for '+channel);
+						}
+					break;
+					case 'off':
+						if(!channel){
+							enabled.global = false;
+							IRC.message(args.channel, 'Logging disabled');
+						}else{
+							enabled.channels[channel] = false
+							IRC.message(args.channel, 'Logging disabled for '+channel);
+						}
+					break;
+				}
+			}
+		}
 	});
 	plugins.listen('Log', 'message', function(args){
 		log('[MSG] ['+args.channel+'] '+args.user+' said: '+args.message, args.channel);
