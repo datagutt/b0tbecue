7d74a36ea1b3c9406286b6e9fd9ba13459a319a2
nebkat <nebkat@teamhacksung.org>
2012-04-06 01:07:56 +0200

    log colors

--- plugins/log.plugin.js
+++ plugins/log.plugin.js
@@ -1,7 +1,23 @@
+var Color = {};
+var Format = {};
+
+Color.BLACK = "\033[30m";
+Color.RED = "\033[31m";
+Color.GREEN = "\033[32m";
+Color.YELLOW = "\033[33m";
+Color.BLUE = "\033[34m";
+Color.MAGENTA = "\033[35m";
+Color.CYAN = "\033[36m";
+Color.WHITE = "\033[31m";
+
+Format.RESET = "\033[0;0m";
+Format.NORMAL = "\033[0m";
+Format.BOLD = "\033[1m";
+
 var fs = require('fs');
 var logged = [];
 var enabled = {global: true, channels: []};
-function log(content, channel){
+function log(content, channel, format){
 	if (enabled.global == false) return;
 	if (enabled.channels[channel] == false) return;
 	var now = new Date();
@@ -21,17 +37,17 @@ function log(content, channel){
 		if(err) {
 			throw err;
 		}else{
-			console.log(original_content);
+			console.log((format ? format : "") + original_content + Format.RESET);
 		}
 	}); 
 }
 exports.init = function(plugins, bot){
 	bot.addCommand('log', 'on/off [channel]', 'Enables/disables logging', USER_LEVEL_OWNER);
 	plugins.listen('Log', 'topic', function(args){
-		log('[TOPIC] '+args.topic, args.channel);
+		log('[TOPIC] '+args.topic, args.channel, Color.MAGENTA);
 	});
 	plugins.listen('Log', 'command', function(args){
-		log('[COM] ['+args.channel+'] '+args.user+' ran command: '+args.command, args.channel);
+		log('[COM] ['+args.channel+'] '+args.user+' ran command: '+args.command, args.channel, Format.BOLD + Color.RED);
 		if(args.command == 'log'){
 			var level = bot.getUserLevel(args.user, args.host);
 			if(!bot.isCommand(args.command, level)){
@@ -66,12 +82,12 @@ exports.init = function(plugins, bot){
 		}
 	});
 	plugins.listen('Log', 'message', function(args){
-		log('[MSG] ['+args.channel+'] '+args.user+' said: '+args.message, args.channel);
+		log('[MSG] ['+args.channel+'] '+args.user+' said: '+args.message, args.channel, Color.GREEN);
 	});
 	plugins.listen('Log', 'join', function(args){
-		log('[JOIN] ['+args.channel+'] '+args.user, args.channel);
+		log('[JOIN] ['+args.channel+'] '+args.user, args.channel, Color.CYAN);
 	});
 	plugins.listen('Log', 'part', function(args){
-		log('[PART] ['+args.channel+'] '+args.user, args.channel);
+		log('[PART] ['+args.channel+'] '+args.user, args.channel, Color.CYAN);
 	});
 };
