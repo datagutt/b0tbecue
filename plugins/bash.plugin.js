237d51623db6680093d58a4e8ebc8d3291f02768
datagutt <datagutt666@hotmail.com>
2012-04-06 22:14:48 +0200

    add quotes to bash

--- plugins/bash.plugin.js
+++ plugins/bash.plugin.js
@@ -1,3 +1,4 @@
+var querystring = require('querystring');
 var http = require('http');
 var bash = function(channel, id){
 	var options = {host: 'bash.org', path: '?' + id}, quote;
@@ -10,8 +11,7 @@ var bash = function(channel, id){
 			if(err){
 				throw err;
 			}
-			data = data.replace(/(\r\n|\n|\r)/gm,"")
-			console.log(data);
+			data = data.replace(/(\r\n|\n|\r)/gm, '');
 			var parsed = data.match(/<p class=\"qt\">(.*?)<\/p>/);
 			quote = parsed ? parsed[1] : '';
 			if(quote){
@@ -28,8 +28,46 @@ var bash = function(channel, id){
 	});
 	return quote;
 }
+var addQuote = function(channel, quote){
+	var post_data = querystring.stringify({
+		'newquote': quote, 
+		'strip': 'on', 
+		'submit1': 'Submit Quote'
+	});
+	var options = {
+		host: 'bash.org', 
+		path: '?add',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': post_data.length
+		}
+	};
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			chunk = chunk.replace(/(\r\n|\n|\r)/gm, '');
+			chunk = chunk.replace(/<br \/>+/g, '\n');
+			var message = chunk.match(/<font class=\"bodytext\">(.*?)<\/font>/i);
+			if(message && message[0]){
+				var id = message[0].match(/(#(.*))/);
+				if(id && id[0]){
+					id = id[0].split('.')[0].replace(/<(?:.|\n)*?>/gm, '');
+					if(/already exists/.exec(message)){
+						IRC.message(channel, 'Quote ' + id + ' already exists!');
+					}else{
+						IRC.message(channel, 'Quote added as ' + id + '.');
+					}
+				}
+			}
+		});
+	});
+	req.write(post_data);
+	req.end();
+}
 exports.init = function(plugins, bot){
 	bot.addCommand('bash', '[<id>]', 'Gets random bash quote or by id', USER_LEVEL_GLOBAL);
+	bot.addCommand('addQuote', '<quote>', 'Adds quote to bash', USER_LEVEL_GLOBAL);
 	plugins.listen(this, 'command', function(args){	
 		switch(args.command){
 			case 'bash':
@@ -39,6 +77,10 @@ exports.init = function(plugins, bot){
 					id = 'random';
 				}
 				bash(args.channel, id);
+			break;
+			case 'addQuote':
+				quote = args.arguments.join(' ');
+				addQuote(args.channel, quote);
 			break;
 		}
 	});
