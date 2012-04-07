a3ae5745b0466a4d3313cbe620cf8745287fcb70
nebkat <nebkat@teamhacksung.org>
2012-04-07 17:24:49 +0200

    uptime cleanup

--- plugins/uptime.plugin.js
+++ plugins/uptime.plugin.js
@@ -6,29 +6,27 @@ var parse = function(uptime){
 	
 	uptime = [];
 	if (days >= 1) {
-		uptime.push('' + days + ' day' + (days > 1 ? 's' : ''));
+		uptime.push(days + ' day' + (days > 1 ? 's' : ''));
 	}
 	if (hours >= 1) {
-		uptime.push('' + hours +' hour' + (hours > 1 ? 's' : ''));
+		uptime.push(hours +' hour' + (hours > 1 ? 's' : ''));
 	}
 	if (minutes >= 1) {
-		uptime.push('' + minutes + ' minute' + (minutes > 1 ? 's' : ''));
+		uptime.push(minutes + ' minute' + (minutes > 1 ? 's' : ''));
 	}
 	if (seconds >= 1) {
-		uptime.push('' + seconds + ' second' + (seconds > 1 ? 's' : ''));
+		uptime.push(seconds + ' second' + (seconds > 1 ? 's' : ''));
 	}
 	uptime = uptime.join(', ');
 	return uptime;
 }
-var uptime = function(bot, channel){
-	var time = (new Date().getTime() - bot.startTime) / 1000;
-	IRC.message(channel, 'Bot has been online for: ' + parse(time));
-}
+
 exports.init = function(plugins, bot){
 	bot.addCommand('uptime', '', 'Shows uptime of bot', USER_LEVEL_GLOBAL, true);
 	plugins.listen(this, 'command', function(args){
-		if(args.command && args.command == 'uptime'){
-			uptime(bot, args.channel);
+		if(args.command == 'uptime'){
+			var time = (new Date().getTime() - bot.startTime) / 1000;
+			IRC.message(args.channel, 'Bot has been online for: ' + parse(time));
 		}
 	});
 }
