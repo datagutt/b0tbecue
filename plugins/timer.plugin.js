f53b7dc17d5d8042bd89090ac4e01a57360f549b
datagutt <datagutt666@hotmail.com>
2012-04-07 20:16:06 +0200

    timer plugin

---
+++ plugins/timer.plugin.js
@@ -0,0 +1,57 @@
+var timers = [];
+exports.init = function(plugins, bot){
+	bot.addCommand('timer', 'time action [<value>]', 'Runs a command using a timer', USER_LEVEL_MOD);
+	var i = 0;
+	plugins.listen(this, 'command', function(args){	
+		switch(args.command){
+			case 'timer':
+				switch(args.arguments[0]){
+					case 'set':
+						if(args.arguments && args.arguments[1] && args.arguments[2]){
+							var time = args.arguments[1] * 1000;
+							var action = args.arguments[2];
+							var value = args.message.split(' ').slice(4);
+							args.command = action;
+							args.message = bot.config.prefix + args.command + ' ' + value;
+							args.arguments = value;
+							timers[i] = [];
+							timers[i].command = args.command;
+							timers[i].arguments = args.arguments;
+							timers[i].number = i;
+							timers[i].action = setTimeout(function(){
+								plugins.fire('command', args);
+								if(timers[i - 1]){
+									timers.splice(i - 1, 1);
+								}
+							}, time);
+							i++;
+							IRC.message(args.channel, 'Timer set!');
+						}
+					break;
+					case 'list':
+						IRC.message(args.channel, 'Timers:');
+						list = '';
+						for(timer in timers){
+							var timer = timers[timer];
+							if(timer){
+								list += timer.number + '[' + timer.command + ' ' + timer.arguments.join(' ') + ']\n';
+							}
+						}
+						IRC.message(args.channel, list);
+					break;
+					case 'cancel':
+						if(args.arguments && args.arguments[1]){
+							for(timer in timers){
+								var timer = timers[timer];
+								if(timer.number == args.arguments[1]){
+									clearTimeout(timer.action);
+									timers.splice(timer.number, 1);
+									IRC.message(args.channel, 'Timer cleared!');
+								}
+							}
+						}
+					break;
+				}
+		}
+	});
+};
