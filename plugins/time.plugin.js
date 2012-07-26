cc1993968b7a63fd85fc2f39e9c1456283368907
datagutt <datagutt666@hotmail.com>
2012-07-26 21:59:27 +0200

    Added worldwatch plugin

---
+++ plugins/time.plugin.js
@@ -0,0 +1,27 @@
+var time = require('time');
+exports.init = function(plugins, bot){
+	bot.addCommand('time', '[<country> <city>]', 'Show time in location', USER_LEVEL_GLOBAL, true);
+	plugins.listen(this, 'command', function(args){
+		var currentTime = new time.Date();
+		if(args.command == 'time'){
+			if (args.arguments && args.arguments[0] && args.arguments[1]) {
+				var country = args.arguments[0];
+				var city = args.arguments[1];
+				
+				currentTime.setTimezone(country + '/' + city);
+	
+				var hours = currentTime.getHours();
+				var minutes = currentTime.getMinutes();
+				var seconds = currentTime.getSeconds();
+				
+				if (minutes < 10){
+					minutes = '0' + minutes;
+				}
+				
+				IRC.message(args.channel, hours + ':' + minutes + ':' + seconds);
+			}else{
+				IRC.message(args.channel, 'Invalid argument!');
+			}
+		}
+	});
+}
