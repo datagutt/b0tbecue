58b1b2d71cdfde06591f9d4d9026e498a2be3671
datagutt <datagutt666@hotmail.com>
2012-10-16 17:18:22 +0200

    Refactored and fixed money plugin

--- plugins/money.plugin.js
+++ plugins/money.plugin.js
@@ -1,35 +1,27 @@
-var fx = require('money');
-var http = require('http');
+var fx = require('money'),
+	exchange = require('exchange-rates');
 var getRates = function(key, callback){
-	var options = {host: 'openexchangerates.org', path: '/latest.json?api_id=' + key}, quote;
-	http.get(options, function(res) {
-		var data = '';
-		res.on('data', function (chunk){
-			data += chunk;
-		});
-		res.on('end',function(err){
-			if(err){
-				throw err;
-			}
-			data = JSON.parse(data);
-			if(typeof callback == 'function'){
-				callback(data);
-			}
-		});
+	exchange.url = 'http://openexchangerates.org/api/latest.json?app_id=' + key;
+	exchange.load(function(){
+		if(typeof callback == 'function'){
+			callback({'rates': exchange.rates, 'base': exchange.base});
+		}
 	});
 }
 exports.init = function(plugins, bot){
+	var self = this;
 	bot.addCommand('money', '<value> <from> <to>', 'Converts currency', USER_LEVEL_GLOBAL);
 	plugins.listen(this, 'command', function(args){	
 		switch(args.command){
 			case 'money':
 				if(args.arguments && args.arguments[0] && args.arguments[1] && args.arguments[2]){
-					var value = parseInt(args.arguments[0]);
+					var value = args.arguments[0];
 					var from = args.arguments[1].toUpperCase();
 					var to = args.arguments[2].toUpperCase();
 					var response;
-					getRates(this.config.key, function(data){
+					getRates(self.config.key, function(data){
 						fx.rates = data.rates;
+						fx.base = data.base;
 						try{
 							var response = fx.convert(value, {'from': from, 'to': to});
 						}catch(e){
